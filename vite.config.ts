721924c099f63e3b8e82bc6cb1f43c0bdccafe92
BalrajHariharanA <balraj@etherscale.xyz>
2025-06-19 14:24:50 +0530

    config pushed

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),reactRefresh()],
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true
-    },
-  },
-    optimizeDeps:{
-    esbuildOptions:{
-      plugins:[
-        esbuildCommonjs(['canvasjs/react-charts'])
-      ]
-    }
-  }
+  plugins: [react(), tailwindcss()],
 })
